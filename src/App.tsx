9485d4b869471e3eff6664e3ab3e53b1aea341c7
lexriacodes <15950907+lexriacodes@users.noreply.github.com>
2025-09-01 18:21:24 -0400

    passing children to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-    "Berlin",
-    "Sydney",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
